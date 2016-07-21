47093086d7857e2a35125a714799d6ef039f161c
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-07-21 10:44:17 +0300

    Add functions for the area of a square and a rectangle

--- sandbox/src/main/java/ru/sfwt/mt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sfwt/mt/sandbox/MyFirstProgram.java
@@ -3,7 +3,27 @@ package ru.sfwt.mt.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hell, world!");
+    hello("word");
+    hello("user");
+    hello("Marina");
 
+    double len = 5;
+    System.out.println("S квадрата со стороной " + len + " = " + area(len));
+
+    double a = 5;
+    double b = 6;
+    System.out.println("S прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hell, " + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 }
