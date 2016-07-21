814b50c68af26ce8e084912e95f201e572b4f354
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-07-21 17:13:48 +0300

    Add methods instead of functions

--- sandbox/src/main/java/ru/sfwt/mt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sfwt/mt/sandbox/MyFirstProgram.java
@@ -8,21 +8,13 @@ public class MyFirstProgram {
     hello("Marina");
 
     Square s = new Square(5);
-    System.out.println("S квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("S квадрата со стороной " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(5,6);
-    System.out.println("S прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    Rectangle r = new Rectangle(5, 6);
+    System.out.println("S прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hell, " + somebody + "!");
-  }
-
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
   }
 }
