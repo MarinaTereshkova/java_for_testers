0f45eae79bd10a490e88f5bc023b69a8767f76a6
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-07-28 17:49:44 +0300

    Group method parameters fill form AddressCreation into object AddressData

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/AddressCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/AddressCreationTests.java
@@ -3,16 +3,11 @@ package ru.sfwt.mt.addressbook;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class AddressCreationTests {
     FirefoxDriver wd;
@@ -22,24 +17,24 @@ public class AddressCreationTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
     public void testAddressCreation() {
         gotoAddreessCreationPage();
-        fillAddressForm();
+        fillAddressForm(new AddressData("name", "last", "Address", "098765", "98765", "87654", "mail_1", "mail_2", "mail_3"));
         enterAddressCreation();
         returnToHomePage();
     }
@@ -52,34 +47,34 @@ public class AddressCreationTests {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillAddressForm() {
+    private void fillAddressForm(AddressData addressData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("name");
+        wd.findElement(By.name("firstname")).sendKeys(addressData.getFirstname());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("last");
+        wd.findElement(By.name("lastname")).sendKeys(addressData.getLastname());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("Address");
+        wd.findElement(By.name("address")).sendKeys(addressData.getAddress());
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("098765");
+        wd.findElement(By.name("home")).sendKeys(addressData.getHomenumber());
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("98765");
+        wd.findElement(By.name("mobile")).sendKeys(addressData.getMobilenumber());
         wd.findElement(By.name("work")).click();
         wd.findElement(By.name("work")).clear();
-        wd.findElement(By.name("work")).sendKeys("87654");
+        wd.findElement(By.name("work")).sendKeys(addressData.getWorknumber());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("mail_1");
+        wd.findElement(By.name("email")).sendKeys(addressData.getEmail1());
         wd.findElement(By.name("email2")).click();
         wd.findElement(By.name("email2")).clear();
-        wd.findElement(By.name("email2")).sendKeys("mail_2");
+        wd.findElement(By.name("email2")).sendKeys(addressData.getEmail2());
         wd.findElement(By.name("email3")).click();
         wd.findElement(By.name("email3")).clear();
-        wd.findElement(By.name("email3")).sendKeys("mail_3");
+        wd.findElement(By.name("email3")).sendKeys(addressData.getEmail3());
     }
 
     private void gotoAddreessCreationPage() {
---
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/AddressData.java
@@ -0,0 +1,61 @@
+package ru.sfwt.mt.addressbook;
+
+public class AddressData {
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String homenumber;
+  private final String mobilenumber;
+  private final String worknumber;
+  private final String email1;
+  private final String email2;
+  private final String email3;
+
+  public AddressData(String firstname, String lastname, String address, String homenumber, String mobilenumber, String worknumber, String email1, String email2, String email3) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.homenumber = homenumber;
+    this.mobilenumber = mobilenumber;
+    this.worknumber = worknumber;
+    this.email1 = email1;
+    this.email2 = email2;
+    this.email3 = email3;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomenumber() {
+    return homenumber;
+  }
+
+  public String getMobilenumber() {
+    return mobilenumber;
+  }
+
+  public String getWorknumber() {
+    return worknumber;
+  }
+
+  public String getEmail1() {
+    return email1;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public String getEmail3() {
+    return email3;
+  }
+}
