aa5e7580606fd849a0ec27de7fa4fc56e9ea2715
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-01 18:16:45 +0300

    Create class assistant ContacHelper. Add method  gotoAddreessCreationPage in class assistant NavigationHelper

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,14 @@
 package ru.sfwt.mt.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.sfwt.mt.addressbook.model.AddressData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   FirefoxDriver wd;
 
+  private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
@@ -30,6 +29,7 @@ public class ApplicationManager {
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
+    contactHelper = new ContactHelper(wd);
     sessionHelper.login("admin", "secret");
   }
 
@@ -38,47 +38,9 @@ public class ApplicationManager {
     wd.quit();
   }
 
-  public void returnToHomePage() {
-    wd.findElement(By.linkText("home")).click();
-  }
-
-  public void enterAddressCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-  }
-
-  public void fillAddressForm(AddressData addressData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(addressData.getFirstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(addressData.getLastname());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(addressData.getAddress());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(addressData.getHomenumber());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(addressData.getMobilenumber());
-    wd.findElement(By.name("work")).click();
-    wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys(addressData.getWorknumber());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(addressData.getEmail1());
-    wd.findElement(By.name("email2")).click();
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys(addressData.getEmail2());
-    wd.findElement(By.name("email3")).click();
-    wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys(addressData.getEmail3());
-  }
-
-  public void gotoAddreessCreationPage() {
-    wd.findElement(By.linkText("add new")).click();
-  }
+  //  public void gotoAddreessCreationPage() {
+//    wd.findElement(By.linkText("add new")).click();
+//  }
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
@@ -86,5 +48,9 @@ public class ApplicationManager {
 
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
+  }
+
+  public ContactHelper getContactHelper() {
+    return contactHelper;
   }
 }
--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressCreationTests.java
@@ -7,10 +7,10 @@ public class AddressCreationTests extends TestBase{
 
   @Test
   public void testAddressCreation() {
-    app.gotoAddreessCreationPage();
-    app.fillAddressForm(new AddressData("name", "last", "Address", "098765", "98765", "87654", "mail_1", "mail_2", "mail_3"));
-    app.enterAddressCreation();
-    app.returnToHomePage();
+    app.getNavigationHelper().gotoAddreessCreationPage();
+    app.getContactHelper().fillAddressForm(new AddressData("name", "last", "Address", "098765", "98765", "87654", "mail_1", "mail_2", "mail_3"));
+    app.getContactHelper().enterAddressCreation();
+    app.getContactHelper().returnToHomePage();
   }
 
 }
