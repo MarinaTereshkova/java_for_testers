b81aac755807f1334078f388686fe3493340c4cd
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-06 00:50:29 +0300

    Provide verification and creation of preconditions for GroupTests

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,17 @@
 package ru.sfwt.mt.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.sfwt.mt.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
