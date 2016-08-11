294c3681b49452ff2fcd38d52a9966700fe01318
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-11 23:04:24 +0300

    Compare lists (before and after) wholly for GroupDeletiionTests. Generate methods toString()&equales()&hashCode() in GroupData
    сравнение списков целиком для теста на удаление группы
    генерация методов toString()&equales()&hashCode() в  GroupData

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ru.sfwt.mt.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod
   public void setUp() throws Exception {
