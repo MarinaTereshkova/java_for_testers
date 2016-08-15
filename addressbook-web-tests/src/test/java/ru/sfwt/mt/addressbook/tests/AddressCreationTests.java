ae4a7c5a9335b43fb04bc08a9d7dbc793fe33367
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-15 20:09:30 +0300

    Create lambda instead of If in AddressCreationTests

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sfwt.mt.addressbook.model.AddressData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,13 +21,17 @@ public class AddressCreationTests extends TestBase{
     List<AddressData> after = app.getContactHelper().getAddressList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (AddressData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    address.setId(max);
+    //int max = 0;
+    //for (AddressData g : after) {
+    //  if (g.getId() > max) {
+    //    max = g.getId();
+    //  }
+    //}
+    //Comparator<? super AddressData> byId = (Comparator<AddressData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    //int max1 = after.stream().max((Comparator<AddressData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+    //address.setId(max1);
+    address.setId(after.stream().max((Comparator<AddressData>) (o1, o2) ->
+            Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(address);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
