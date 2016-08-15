2ddce7585025b797df40aa622400aaca4ce6f207
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-15 18:47:32 +0300

    Compare list size for AddressModification&DeletionTests

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressDeletionTests.java
@@ -5,21 +5,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sfwt.mt.addressbook.model.AddressData;
 
+import java.util.List;
+
 public class AddressDeletionTests extends TestBase {
 
   @Test
   public void testAddressDeletion (){
     app.getNavigationHelper().gotoHomePage();
-    int before = app.getContactHelper().getAddressCount();
+    List<AddressData> before = app.getContactHelper().getAddressList();
     if (! app.getContactHelper().isThereAnAderess()) {
       app.getNavigationHelper().gotoAddreessCreationPage();
       app.getContactHelper().createAddress(new AddressData("name", "last", null, null, null, null, null, null, null,"test1"));
     }
-    app.getContactHelper().selectAddress(before - 1);
+    app.getContactHelper().selectAddress(before.size() - 1);
     app.getContactHelper().deleteSelectAddress();
     app.getContactHelper().returnToHomePage();
-    int after = app.getContactHelper().getAddressCount();
-    Assert.assertEquals(after, before - 1);
+    List<AddressData> after = app.getContactHelper().getAddressList();
+    Assert.assertEquals(after.size(), before.size() - 1);
 
   }
 }
--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressModificationTests.java
@@ -4,21 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sfwt.mt.addressbook.model.AddressData;
 
+import java.util.List;
+
 public class AddressModificationTests extends TestBase {
 
   @Test
   public void testAddressModification() {
     app.getNavigationHelper().gotoHomePage();
-    int before = app.getContactHelper().getAddressCount();
+    List<AddressData> before = app.getContactHelper().getAddressList();
     if (! app.getContactHelper().isThereAnAderess()) {
       app.getNavigationHelper().gotoAddreessCreationPage();
       app.getContactHelper().createAddress(new AddressData("name", "last", null, null, null, null, null, null, null,"test1"));
     }
-    app.getContactHelper().selectAddress(before - 1);
+    app.getContactHelper().selectAddress(before.size() - 1);
     app.getContactHelper().fillAddressForm(new AddressData("name", "last", "Address", "098765", "98765", "87654", "mail_1", "mail_2", "mail_3", null), false);
     app.getContactHelper().submitAddressModification();
     app.getContactHelper().returnToHomePage();
-    int after = app.getContactHelper().getAddressCount();
-    Assert.assertEquals(after, before);
+    List<AddressData> after = app.getContactHelper().getAddressList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
