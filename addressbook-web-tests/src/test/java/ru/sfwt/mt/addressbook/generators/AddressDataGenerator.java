b7162f9d95845561c615038f3358099467b400d7
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-26 18:51:36 +0300

    Start generate with parameters for AddressDataGenerator. JCommander's library
    Lesson6
    3. Анализ опций командой строки при запуске генератора данных

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/generators/AddressDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/generators/AddressDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.sfwt.mt.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.sfwt.mt.addressbook.model.AddressData;
 
 import java.io.File;
@@ -10,15 +13,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddressDataGenerator {
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Address count")
+  public int count;
 
-    List<AddressData> adresses = generateAddress(count);
-    save(adresses, file);
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    AddressDataGenerator generator = new AddressDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<AddressData> adresses, File file) throws IOException {
+  private void run() throws IOException {
+    List<AddressData> adresses = generateAddress(count);
+    save(adresses, new File(file));
+  }
+
+  private void save(List<AddressData> adresses, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (AddressData address : adresses) {
       writer.write(String.format(("%s;%s\n"), address.getFirstname(), address.getLastname()));
@@ -26,7 +44,7 @@ public class AddressDataGenerator {
     writer.close();
   }
 
-  private static List<AddressData> generateAddress(int count) {
+  private List<AddressData> generateAddress(int count) {
     List<AddressData> adresses = new ArrayList<AddressData>();
     for (int i = 0; i < count; i++) {
       adresses.add(new AddressData().withFirstname(String.format("firstName %s", i))
