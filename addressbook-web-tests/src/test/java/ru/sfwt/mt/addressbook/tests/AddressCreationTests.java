3077d94caeaad4623e44f3f3255fecf41554f1ad
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-27 17:17:07 +0300

    Create @DataProvider in AddressCreationTests
    Lesson6_4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/AddressCreationTests.java
@@ -1,22 +1,47 @@
 package ru.sfwt.mt.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.sfwt.mt.addressbook.model.AddressData;
 import ru.sfwt.mt.addressbook.model.Addresses;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AddressCreationTests extends TestBase{
 
-  @Test
-  public void testAddressCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContact() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new AddressData().withFirstname("name1").withLastname("last1").withGroup("test1")});
+    list.add(new Object[] {new AddressData().withFirstname("name2").withLastname("last2").withGroup("test1")});
+    list.add(new Object[] {new AddressData().withFirstname("name2").withLastname("last2").withGroup("test1")});
+    return list.iterator();
+
+  }
+
+  @Test(dataProvider = "validContact")
+  public void testAddressCreation(AddressData address) {
     app.goTo().homePage();
     Addresses before = app.contact().all();
     app.goTo().addressCreationPage();
-    //AddressData address = new AddressData().withFirstname("name").withLastname("last").withGroup("test1");
+    app.contact().create(address);
+    app.goTo().homePage();
+    assertThat(app.contact().count(), equalTo(before.size() +1));
+    Addresses after = app.contact().all();
+    assertThat(after, equalTo(before.withAdded(
+            address.withId(after.stream().mapToInt((ad) -> ad.getId()).max().getAsInt()))));
+  }
+  @Test
+  public void testAddressCreationPhoto() {
+    app.goTo().homePage();
+    Addresses before = app.contact().all();
+    app.goTo().addressCreationPage();
     File photo = new File("/src/test/resources/images.jpg");
     AddressData address = new AddressData().withFirstname("name").withLastname("last").withPhoto(photo).withGroup("test1");
     app.contact().create(address);
