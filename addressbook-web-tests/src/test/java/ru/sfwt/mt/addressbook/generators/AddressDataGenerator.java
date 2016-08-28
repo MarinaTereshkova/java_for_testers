e8ac39606c072213f2162c9820e6d3910cd88576
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-28 14:45:29 +0300

    Save test data in xml-file. Add xml format in AddressDataGenerator
    Lesson6
    6. Формат XML

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/generators/AddressDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/generators/AddressDataGenerator.java
@@ -3,6 +3,7 @@ package ru.sfwt.mt.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.sfwt.mt.addressbook.model.AddressData;
 
 import java.io.File;
@@ -19,6 +20,9 @@ public class AddressDataGenerator {
   @Parameter(names = "-f", description = "Target file")
   public String file;
 
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
   public static void main(String[] args) throws IOException {
     AddressDataGenerator generator = new AddressDataGenerator();
     JCommander jCommander = new JCommander(generator);
@@ -33,10 +37,27 @@ public class AddressDataGenerator {
 
   private void run() throws IOException {
     List<AddressData> adresses = generateAddress(count);
-    save(adresses, new File(file));
+    if (format.equals("csv")) {
+      saveAsCsv(adresses, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(adresses, new File(file));
+    } else {
+      System.out.println("Unrecognized format " + format);
+    }
+
   }
 
-  private void save(List<AddressData> adresses, File file) throws IOException {
+  private void saveAsXml(List<AddressData> adresses, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(AddressData.class);
+    String xml = xstream.toXML(adresses);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+
+  }
+
+  private void saveAsCsv(List<AddressData> adresses, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (AddressData address : adresses) {
       writer.write(String.format(("%s;%s\n"), address.getFirstname(), address.getLastname()));
