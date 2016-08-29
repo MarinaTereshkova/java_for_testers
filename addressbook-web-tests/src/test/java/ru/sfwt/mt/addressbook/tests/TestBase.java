3609a09913d57782ed218dc3438b578567403bb2
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-08-29 19:32:49 +0300

    Create system properties for run test in console with keys: -Pbrowser and -Ptarget
    Lesson6
    10. Описание конфигурации тестового стенда
    - запуск браузера из командной строки:
    gradlew -Ptarget=local testContats
    - запуск файла с конфигом тестового стенда:
    gradlew -Ptarget=local testContacts

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/TestBase.java
@@ -5,9 +5,11 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.sfwt.mt.addressbook.appmanager.ApplicationManager;
 
+import static java.lang.System.*;
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
