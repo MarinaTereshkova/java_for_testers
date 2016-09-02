1e82f8a64c92ebf8b5db638e5c72662ffcb19c2f
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-09-02 20:06:19 +0300

    Create DpHelper. Change Precondition for GroupModificationTests- test data from DB
    Lesson7
    4. Сравнение данных, загруженных из БД

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.sfwt.mt.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import ru.sfwt.mt.addressbook.model.GroupData;
@@ -8,25 +9,33 @@ import ru.sfwt.mt.addressbook.model.Groups;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class GroupModificationTests extends TestBase{
+public class GroupModificationTests extends TestBase {
 
-  @BeforeTest
+ // @BeforeTest
+ // public void ensurePrecondition() {
+ //   app.goTo().groupPage();
+ //   if (app.group().all().size() == 0) {
+ //     app.group().create(new GroupData().withName("test1"));
+ //   }
+ // }
+  @BeforeMethod
   public void ensurePrecondition() {
-    app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
       app.group().create(new GroupData().withName("test1"));
     }
   }
 
   @Test
-  public void testGroupModification(){
-    Groups before = app.group().all();
+  public void testGroupModification() {
+    Groups before = app.db().groups();
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData()
             .withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
+    app.goTo().groupPage();
     app.group().modify(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
