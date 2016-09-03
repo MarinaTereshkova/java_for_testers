d06a31ea059ebdd9f2f120d0754f9bf8356e70f2
MarinaTereshkova <tereshkova.ms@gmail.com>
2016-09-03 13:40:54 +0300

    Create cheking lisf form DB and from UI. Compare results. Add checking in GroupTests
    Lesson7
    5. Отключаемые проверки UI
    
    -довалена проверка загруженных списков из UI и из DB:
    verifyGroupListInUI() в TestBase
    -провека отключаемая через EditConfigurations: -DverifyUI=true -
    выполнять проверку
    -добавлена возможность выполнять проверку через консоль

--- addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/sfwt/mt/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package ru.sfwt.mt.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,11 +10,16 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.sfwt.mt.addressbook.appmanager.ApplicationManager;
+import ru.sfwt.mt.addressbook.model.GroupData;
+import ru.sfwt.mt.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static java.lang.System.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -40,4 +47,13 @@ public class TestBase {
     logger.info("Stop test " + m.getName());
   }
 
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI")) {
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
+  }
 }
